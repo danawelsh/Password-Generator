0dbe4972421c5b8df5a9bea3ccd41d20187955f6
danawelsh <111257868+danawelsh@users.noreply.github.com>
2022-12-12 21:09:42 -0500

    modified javascript

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var chars = "0123456789" "abcdefghijklmnopqrstuvwxyz" "!@#$%^&*" "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 12;
+  var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+     var randomNumber = Math.floor(Math.random() * chars.length);
+     password += chars.substring(randomNumber, randomNumber +1);
+     }
+          document.getElementById("password").value = password;
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  
