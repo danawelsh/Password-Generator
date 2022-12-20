66fe65b095d711882dfc5e6ee8fe8e264e6d563b
danawelsh <111257868+danawelsh@users.noreply.github.com>
2022-12-19 22:11:46 -0500

    made changes to javascript file

--- script.js
+++ script.js
@@ -11,21 +11,27 @@ function generatePassword() {
   var password = "";
   // get user input
   var passwordLength = prompt("how many characters do you want to include in your password?")
-  var characterTypes = prompt("Which character types would you like to include in your password?")
-  var incNums = prompt("Do you want to include a number?")
-  var incLower = prompt ("Do you want to include a lowercase character?")
-  var incUpper = prompt ("Do you want to include an uppercase character?")
-  var incSpecialCharacter = prompt ("Do you want to include special characters?")
+  var incNums = confirm("Do you want to include a number?")
+  var incLower = confirm ("Do you want to include a lowercase character?")
+  var incUpper = confirm ("Do you want to include an uppercase character?")
+  var incSpecialCharacter = confirm ("Do you want to include special characters?")
 
-  if incNums == True
-    characterTypes = nums
-  if incLower == True
-    characterTypes = incLower
+  var chars = "";
+  if (incNums == true) {
+    chars += nums
+  }
+  if (incLower == true) {
+    chars += lowercase
+  }
+  if (incUpper == true) {
+    chars += uppercase
+  }
+  if (incSpecialCharacter == true) {
+    chars += spcialCharacter
+  }
 
 
-  // using the input, generate a pool of characters to select from
-  //var pool = "if/else statements"
-  // after generating the pool, then you can do the for loop
+  // 
 
   for (var i = 0; i <= passwordLength; i++) {
   var randomNumber = Math.floor(Math.random() * chars.length);
@@ -45,8 +51,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-passwordBtnEl.on('click', function () {
-  var newPassword = passwordGenerator(8);
-  passwordDisplayEl.text(newPassword);
-});
   
